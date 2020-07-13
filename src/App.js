a7eb1fa7cd30b711596d91a8c7b72d25c7aad4f5
fathanfatazka <fathan_fatazka@apps.ipb.ac.id>
2020-07-13 14:32:30 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <h1>Hello...</h1>
+       <p>Saya cuma bisa lancar sampai sini :)</p>
+       <p>"Done is Better Than Perfect" -Mentor</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
